11e4ab6cc8f4100c4b0ea35ad2d9aa05a502d418
degel55 <chicubfan@gmail.com>
2016-09-01 21:23:28 -0500

    REST work

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,37 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    
+  end
+
+  def create
+    @recipe = Recipe.create(title: params[:title],
+                            chef: params[:chef],
+                            prep_time: params[:prep_time],
+                            ingredients: params[:ingredients],
+                            directions: params[:directions])
+    render 'show.html.erb'                                                        
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(title: params[:title],
+                   prep_time: params[:prep_time],
+                   chef: params[:chef],
+                   ingredients: params[:ingredients],   
+                   directions: params[:directions])   
+    
+    render 'show.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'recipes#index'
+  get '/recipes' => 'recipes#index'
+
+  get 'recipes/new' => 'recipes#new'
+  post '/recipes' => 'recipes#create'
+
+  get '/recipes/:id' => 'recipes#show'
+
+  get 'recipes/:id/edit' => 'recipes#edit'
+  patch '/recipes/:id' => 'recipes#update'
 end
