a7be847d0c3623c8a5d960445ad287056879ebfc
degel55 <chicubfan@gmail.com>
2016-09-06 22:17:47 -0500

    EOD

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,7 +17,9 @@ class RecipesController < ApplicationController
                             prep_time: params[:prep_time],
                             ingredients: params[:ingredients],
                             directions: params[:directions])
-    render 'show.html.erb'                                                        
+
+    flash[:success] = "New Recipe Created"
+    redirect_to "/recipes/#{@recipe.id}"                                                        
   end
 
   def edit
@@ -32,6 +34,15 @@ class RecipesController < ApplicationController
                    ingredients: params[:ingredients],   
                    directions: params[:directions])   
     
-    render 'show.html.erb'
+    flash[:success] = "Recipe Updated"
+    redirect_to "/recipes/#{@recipe.id}"
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    
+    flash[:warning] = "Recipe Deleted"
+    redirect_to '/recipes'
   end
 end
