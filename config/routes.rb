9622601ff2b13c2313586d0badd06fbf3eaf8907
degel55 <chicubfan@gmail.com>
2016-09-13 19:37:56 -0500

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
   patch '/recipes/:id' => 'recipes#update'
 
   delete '/recipes/:id' => 'recipes#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 end
